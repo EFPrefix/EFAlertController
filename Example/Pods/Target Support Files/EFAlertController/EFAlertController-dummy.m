ac1945bebb5f347ed18fabed5220216d8556f234
EyreFree <zheng.xu03@ele.me>
2017-04-11 21:47:01 +0800

    Setup

---
+++ Example/Pods/Target Support Files/EFAlertController/EFAlertController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EFAlertController : NSObject
+@end
+@implementation PodsDummy_EFAlertController
+@end
---
+++ Example/Pods/Target Support Files/Pods-EFAlertController_Example/Pods-EFAlertController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EFAlertController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_EFAlertController_Example
+@end
